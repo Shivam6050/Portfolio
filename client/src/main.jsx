d3bd851b744949d64cff41b4e47be3ad4b8543c9
Shivam Sagar <160846295+Shivam6050@users.noreply.github.com>
2026-09-24 01:54:17 +0530

    refactor: load page stylesheet separately

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { AppProvider } from "./context/AppContext.jsx";
 import "./index.css";
+import "./styles/portfolio.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
